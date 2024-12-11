e8365a17ba77bb75c34b2f81421a57a76e3798d4
TiesKlaver1010 <144677142+TiesKlaver1010@users.noreply.github.com>
2024-12-11 12:04:00 +0100

    Switch
    mooie switch mechanic

---
+++ Assets/Scripts/SystemManagers/PokemonSwitch.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonSwitch : MonoBehaviour
+{
+    [SerializeField] GameObject pokemonUI;
+    [SerializeField] GameObject SnorlaxUI;
+    [SerializeField] GameObject SnorlaxSpawn;
+    [SerializeField] GameObject ToxtricitySpawn;
+    public void Snorlax()
+    {
+        SnorlaxSpawn.SetActive(true);
+        ToxtricitySpawn.SetActive(false);
+    }
+    public void PokemonSelector()
+    { 
+        pokemonUI.SetActive(true);
+        SnorlaxUI.SetActive(true);
+    }
+    public void pokemonDeSelector() 
+    {
+        pokemonUI.SetActive(false);
+        SnorlaxUI.SetActive(false);
+    }
+    public void Toxtricity() 
+    {
+        SnorlaxSpawn.SetActive(false);
+        ToxtricitySpawn.SetActive(true);
+    }
+}
